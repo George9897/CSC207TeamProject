0805f6665381fb8911ae0ad3ebde99846782975e
yangzhey <yang.zheyu@mail.utoronto.ca>
2018-11-24 00:10:17 -0500

    NEW UNIT TEST CLASSES FOR MINE GAME STYLE FIX

---
+++ Phase2/GameCenter/app/test/fall2018/csc2017/GameCenter/MineTileTest.java
@@ -0,0 +1,54 @@
+package fall2018.csc2017.GameCenter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MineTileTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getValue() {
+    }
+
+    @Test
+    public void getIsOpened() {
+    }
+
+    @Test
+    public void getX() {
+    }
+
+    @Test
+    public void getY() {
+    }
+
+    @Test
+    public void getBackground() {
+    }
+
+    @Test
+    public void setValue() {
+    }
+
+    @Test
+    public void setX() {
+    }
+
+    @Test
+    public void setY() {
+    }
+
+    @Test
+    public void setBackground() {
+    }
+}
