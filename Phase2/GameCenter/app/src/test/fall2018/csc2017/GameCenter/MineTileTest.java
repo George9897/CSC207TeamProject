2e2db01c60922ac1e2543f2e6bbf879e02880ed4
yangzhey <yang.zheyu@mail.utoronto.ca>
2018-11-25 02:40:18 -0500

    javadoc added

--- Phase2/GameCenter/app/src/test/fall2018/csc2017/GameCenter/MineTileTest.java
+++ Phase2/GameCenter/app/src/test/fall2018/csc2017/GameCenter/MineTileTest.java
@@ -6,6 +6,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+/**
+ * Test for mine tile class.
+ */
 public class MineTileTest {
 
     @Before
@@ -16,38 +19,65 @@ public class MineTileTest {
     public void tearDown() throws Exception {
     }
 
+    /**
+     * Test whether the getValue works.
+     */
     @Test
     public void getValue() {
     }
 
+    /**
+     * Test whether the getIsOpened works.
+     */
     @Test
     public void getIsOpened() {
     }
 
+    /**
+     * Test whether the getX works.
+     */
     @Test
     public void getX() {
     }
 
+    /**
+     * Test whether the getY works.
+     */
     @Test
     public void getY() {
     }
 
+    /**
+     * Test whether the getBackground works.
+     */
     @Test
     public void getBackground() {
     }
 
+    /**
+     * Test whether the setValue works.
+     */
     @Test
     public void setValue() {
     }
 
+    /**
+     * Test whether the setX works.
+     */
     @Test
     public void setX() {
     }
 
+    /**
+     * Test whether the setY works.
+     */
     @Test
     public void setY() {
     }
 
+    /**
+     * Test whether the setBackground works.
+     */
     @Test
     public void setBackground() {
     }
