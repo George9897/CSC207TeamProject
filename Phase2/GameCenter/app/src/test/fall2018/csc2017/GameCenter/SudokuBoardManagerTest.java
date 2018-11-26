3b7fc31e956815c162b047b6a93f945a441dcd45
Yuan Gao <gaoyuan0227@gmail.com>
2018-11-25 21:00:20 -0500

    Add Test to the Sudoku game.

---
+++ Phase2/GameCenter/app/src/test/fall2018/csc2017/GameCenter/SudokuBoardManagerTest.java
@@ -0,0 +1,90 @@
+package fall2018.csc2017.GameCenter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SudokuBoardManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getTime() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+
+    @Test
+    public void getSudokuDifficulty() {
+    }
+
+    @Test
+    public void setSudokuDifficulty() {
+    }
+
+    @Test
+    public void setTime() {
+    }
+
+    @Test
+    public void getSudoku() {
+    }
+
+    @Test
+    public void getUserName() {
+    }
+
+    @Test
+    public void getDifficulty() {
+    }
+
+    @Test
+    public void createTiles() {
+    }
+
+    @Test
+    public void getSudokuBoardManager() {
+    }
+
+    @Test
+    public void destroySudokuBoardManager() {
+    }
+
+    @Test
+    public void puzzleSolved() {
+    }
+
+    @Test
+    public void wining() {
+    }
+
+    @Test
+    public void isValidTap() {
+    }
+
+    @Test
+    public void makeMove() {
+    }
+
+    @Test
+    public void clear() {
+    }
+
+    @Test
+    public void setMove() {
+    }
+
+    @Test
+    public void undo() {
+    }
+}
