4ea522323b5aa520c818d98bf5c145e963125521
yangzhey <yang.zheyu@mail.utoronto.ca>
2018-11-25 21:41:25 -0500

    Scorer test finished

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/MineScorerTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/MineScorerTest.java
@@ -4,21 +4,28 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.Serializable;
-
 import static org.junit.Assert.*;
 
 /**
  * Test for mine scorer class.
  */
 public class MineScorerTest {
+    private MineScorer mineScorer;
 
+    /**
+     * Set up a scorer for testing it.
+     */
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        mineScorer = new MineScorer();
     }
 
+    /**
+     * Tear down.
+     */
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
+        mineScorer = null;
     }
 
     /**
@@ -26,5 +33,7 @@ public class MineScorerTest {
      */
     @Test
     public void testCalculateScore() {
+        assertEquals(26000, mineScorer.calculateScore(26,0));
+        assertEquals(24728, mineScorer.calculateScore(26, 10));
     }
 }
