c09c632683a42cfe5c7aa43f1efc08094647ef20
Yuan Gao <gaoyuan0227@gmail.com>
2018-11-26 22:46:16 -0500

    improve SudokuBoardManager

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/SudokuBoardManagerTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/SudokuBoardManagerTest.java
@@ -1,5 +1,8 @@
 package fall2018.csc2017.GameCenter;
 
+import android.content.Context;
+import android.test.mock.MockContext;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,44 +11,71 @@ import static org.junit.Assert.*;
 
 public class SudokuBoardManagerTest {
 
+    /**
+     * The SudokuBoardManager for test.
+     */
+    private SudokuBoardManager sudokuBoardManager;
+
+    /**
+     * The AccountManager.
+     */
+    private AccountManager accountManager;
+
+    /**
+     * The user's name.
+     */
+    private String userName;
+
+    /**
+     * Context for test.
+     */
+    Context context = new MockContext();
+
     @Before
     public void setUp() throws Exception {
+        sudokuBoardManager = new SudokuBoardManager(context,"Easy");
     }
 
     @After
     public void tearDown() throws Exception {
+        sudokuBoardManager = null;
     }
 
     @Test
     public void getTime() {
+        assertEquals(0, sudokuBoardManager.getTime());
     }
 
     @Test
     public void getScore() {
+        assertEquals(0, sudokuBoardManager.getScore());
     }
 
     @Test
     public void getSudokuDifficulty() {
-    }
-
-    @Test
-    public void setSudokuDifficulty() {
+        assertEquals("Easy", sudokuBoardManager.getSudokuDifficulty());
     }
 
     @Test
     public void setTime() {
+        sudokuBoardManager.setTime(19);
+        assertEquals(19 , sudokuBoardManager.getTime());
+
     }
 
     @Test
     public void getSudoku() {
+//        assertEquals(sudokuBoardManager.udoku, sudokuBoardManager.getSudoku());
     }
 
     @Test
     public void getUserName() {
+        assertEquals("Easy", sudokuBoardManager.getUserName());
     }
 
     @Test
     public void getDifficulty() {
+        assertEquals("Easy", sudokuBoardManager.getSudokuDifficulty());
     }
 
     @Test
@@ -54,6 +84,8 @@ public class SudokuBoardManagerTest {
 
     @Test
     public void getSudokuBoardManager() {
+        assertEquals("Easy", sudokuBoardManager.getSudokuDifficulty());
+
     }
 
     @Test
