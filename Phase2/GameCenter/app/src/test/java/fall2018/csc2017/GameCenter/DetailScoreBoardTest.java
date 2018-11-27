50c36e6279d70b17294eacce5c39f54379d435b6
tsin <tingxin.xie@mail.utoronto.ca>
2018-11-27 00:39:30 -0500

    Create DetailScoreBoardTest.

---
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/DetailScoreBoardTest.java
@@ -0,0 +1,254 @@
+package fall2018.csc2017.GameCenter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DetailScoreBoardTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getBoardManager() {
+    }
+
+    @Test
+    public void setBoardManager() {
+    }
+
+    @Test
+    public void getSudokuBoardManager() {
+    }
+
+    @Test
+    public void setSudokuBoardManager() {
+    }
+
+    @Test
+    public void getMineManager() {
+    }
+
+    @Test
+    public void setMineManager() {
+    }
+
+    @Test
+    public void destroyAllManager() {
+    }
+
+    @Test
+    public void setContext() {
+    }
+
+    @Test
+    public void getContext() {
+    }
+
+    @Test
+    public void getLevel() {
+    }
+
+    @Test
+    public void setLevel() {
+    }
+
+    @Test
+    public void getUsername() {
+    }
+
+    @Test
+    public void setUsername() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+
+    @Test
+    public void setScore() {
+    }
+
+    @Test
+    public void collectScoreLevel() {
+    }
+
+    @Test
+    public void createSortedList() {
+    }
+
+    @Test
+    public void getEasyLevel() {
+    }
+
+    @Test
+    public void setEasyLevel() {
+    }
+
+    @Test
+    public void getEasyTopOneName() {
+    }
+
+    @Test
+    public void setEasyTopOneName() {
+    }
+
+    @Test
+    public void getEasyTopOneScore() {
+    }
+
+    @Test
+    public void setEasyTopOneScore() {
+    }
+
+    @Test
+    public void getEasyScoreList() {
+    }
+
+    @Test
+    public void setEasyScoreList() {
+    }
+
+    @Test
+    public void modifyEasyTopOne() {
+    }
+
+    @Test
+    public void getMediumLevel() {
+    }
+
+    @Test
+    public void setMediumLevel() {
+    }
+
+    @Test
+    public void getMediumTopOneName() {
+    }
+
+    @Test
+    public void setMediumTopOneName() {
+    }
+
+    @Test
+    public void getMediumTopOneScore() {
+    }
+
+    @Test
+    public void setMediumTopOneScore() {
+    }
+
+    @Test
+    public void getMediumScoreList() {
+    }
+
+    @Test
+    public void setMediumScoreList() {
+    }
+
+    @Test
+    public void modifyMediumTopOne() {
+    }
+
+    @Test
+    public void getHardLevel() {
+    }
+
+    @Test
+    public void setHardLevel() {
+    }
+
+    @Test
+    public void getHardTopOneScore() {
+    }
+
+    @Test
+    public void setHardTopOneScore() {
+    }
+
+    @Test
+    public void getHardTopOneName() {
+    }
+
+    @Test
+    public void setHardTopOneName() {
+    }
+
+    @Test
+    public void getHardScoreList() {
+    }
+
+    @Test
+    public void setHardScoreList() {
+    }
+
+    @Test
+    public void modifyHardTopOne() {
+    }
+
+    @Test
+    public void getEasyTopOne() {
+    }
+
+    @Test
+    public void getMediumTopOne() {
+    }
+
+    @Test
+    public void getHardTopOne() {
+    }
+
+    @Test
+    public void getEasyMap() {
+    }
+
+    @Test
+    public void setEasyMap() {
+    }
+
+    @Test
+    public void getGameType() {
+    }
+
+    @Test
+    public void setGameType() {
+    }
+
+    @Test
+    public void getEasySortedList() {
+    }
+
+    @Test
+    public void getMediumMap() {
+    }
+
+    @Test
+    public void setMediumMap() {
+    }
+
+    @Test
+    public void getMediumSortedList() {
+    }
+
+    @Test
+    public void getHardMap() {
+    }
+
+    @Test
+    public void setHardMap() {
+    }
+
+    @Test
+    public void getHardSortedList() {
+    }
+
+    @Test
+    public void getHighestScoreByUser() {
+    }
+}
