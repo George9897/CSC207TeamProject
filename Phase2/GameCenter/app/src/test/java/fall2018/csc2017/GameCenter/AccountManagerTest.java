a159a33366e88385cc44e4c8fd6eec89dddaff71
Yuan Gao <gaoyuan0227@gmail.com>
2018-11-28 22:21:31 -0500

    improve test for AccountManager

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/AccountManagerTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/AccountManagerTest.java
@@ -17,35 +17,44 @@ public class AccountManagerTest {
      */
     private AccountManager accountManager;
     private Context context;
-    private String userName = "Tom";
-
 
     @Before
-    public void setUpCorrect() throws Exception {
-//        Context context = mock(Context.class);
-        Context context = mock(Context.class);
+    public void setUpCorrect(){
+        context = Mockito.mock(Context.class);
         accountManager = new AccountManager(context);
-        accountManager.setUp(userName,"tom123");
+
     }
 
     @After
     public void tearDown() throws Exception {
+        context = null;
         accountManager = null;
     }
 
     @Test
     public void setUp() {
+//        setUpCorrect();
+//        accountManager.setUp("Tom","tom123");
+
     }
 
     @Test
     public void login() {
+//        accountManager.login("Tom");
     }
 
     @Test
     public void checkUsername() {
+//        accountManager.setUp("Tom","tom123");
+//        accountManager.setUserName("Tom");
+//        assertTrue(accountManager.checkUsername("Tom"));
+//        assertFalse(accountManager.checkUsername("Jerry"));
     }
 
     @Test
     public void checkPassword() {
+//        accountManager.setUp("Tom","tom123");
+//        assertTrue(accountManager.checkPassword("Tom","tom123"));
+//        assertFalse(accountManager.checkPassword("Tom","jerry123"));
     }
 }
