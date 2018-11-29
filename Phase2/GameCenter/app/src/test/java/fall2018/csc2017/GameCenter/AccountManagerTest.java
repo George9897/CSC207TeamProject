13e07e7ffdc6dabfc128fb6724d3cacac3dd99ce
Yuan Gao <gaoyuan0227@gmail.com>
2018-11-29 02:03:32 -0500

    improve the test for AccountManager

--- Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/AccountManagerTest.java
+++ Phase2/GameCenter/app/src/test/java/fall2018/csc2017/GameCenter/AccountManagerTest.java
@@ -1,15 +1,12 @@
 package fall2018.csc2017.GameCenter;
 
 import android.content.Context;
-import android.test.mock.MockContext;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
 
 public class AccountManagerTest {
     /**
@@ -22,39 +19,19 @@ public class AccountManagerTest {
     public void setUpCorrect(){
         context = Mockito.mock(Context.class);
         accountManager = new AccountManager(context);
-
+        accountManager.setUserName("tom");
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown(){
         context = null;
         accountManager = null;
     }
 
     @Test
     public void setUp() {
-//        setUpCorrect();
-//        accountManager.setUp("Tom","tom123");
-
-    }
-
-    @Test
-    public void login() {
-//        accountManager.login("Tom");
-    }
-
-    @Test
-    public void checkUsername() {
-//        accountManager.setUp("Tom","tom123");
-//        accountManager.setUserName("Tom");
-//        assertTrue(accountManager.checkUsername("Tom"));
-//        assertFalse(accountManager.checkUsername("Jerry"));
-    }
-
-    @Test
-    public void checkPassword() {
-//        accountManager.setUp("Tom","tom123");
-//        assertTrue(accountManager.checkPassword("Tom","tom123"));
-//        assertFalse(accountManager.checkPassword("Tom","jerry123"));
+        context = Mockito.mock(Context.class);
+        accountManager = new AccountManager(context);
+        accountManager.setUserName("tom");
     }
 }
